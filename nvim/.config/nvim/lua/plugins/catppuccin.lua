9808e4ad254aa658ca2592baeab910d5dc84c004
Alexey Dvoryanskiy <advoryanskiy@gmail.com>
2024-03-06 12:28:06 +0400

    gitignore and formatting update

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,20 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("catppuccin")
-  end,
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 10000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  --  {
+  --    "doums/darcula",
+  --    lazy = false,
+  --    name = "darcula",
+  --    priority = 1000,
+  --    config = function()
+  --      vim.cmd.colorscheme("darcula")
+  --    end,
+  --  }
 }
